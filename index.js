5cd696c0105990358a78fbc2dc17683271a5c4fb
lindellcarternyc <lindellcarternyc@gmail.com>
2021-01-20 09:10:44 -0500

    solve task 7

--- index.js
+++ index.js
@@ -164,8 +164,14 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(array, filterWord){
+    const filtered = []
+    array.forEach(word => {
+        if (word.includes(filterWord)) {
+            filtered.push(word)
+        }
+    })
+    return filtered
 }
 
 
